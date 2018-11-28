2c61fcb6991a1729a0432bd21dfcf7f634cbf95d
Jennifer Johnson <jennifer@storj.io>
2018-11-28 02:20:43 -0500

    rearranging

--- pkg/accounting/dbx/gen.go
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2018 Storj Labs, Inc.
-// See LICENSE for copying information.
-
-package accounting
-
-// dbx.v1 schema -d postgres -d sqlite3 accounting.dbx .
-// dbx.v1 golang -d postgres -d sqlite3 accounting.dbx .
